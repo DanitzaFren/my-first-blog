37fb244c9da36f93663887ed59cdfd5b46509e15
daniifreen <d.fren@alumnos.duoc.cl>
2018-10-28 17:30:34 -0300

    setiing

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0u-$6#lxq83cioc2mixtv(fx^6zzmhay89*$47a&-gbwkh97n0'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['127.0.0.1', ' . pythonanywhere.com']
+ALLOWED_HOSTS = ['127.0.0.1', 'community.pythonanywhere.com']
 
 
 # Application definition
